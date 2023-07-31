72dcfb3edeaf85c6c7b50234c00e6036775563e7
Mohamed Adel <mohamedqwe542@gmail.com>
2023-07-31 03:14:26 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,90 @@
+const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+
+const IMGPATH = 'https://image.tmdb.org/t/p/w1280'
+
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query='
+const main = document.querySelector('main');
+const form = document.querySelector('form');
+const search = document.querySelector('#search');
+
+
+
+
+getMovies(APIURL)
+
+
+async function getMovies(url){
+  const resp = await fetch(url);
+  const responseData = await resp.json();
+  showMovies(responseData)
+  
+  return responseData;
+}
+
+function showMovies(movies){
+
+  //clear main
+  main.innerHTML = ''
+  movies.results.forEach(movie => {
+    const {poster_path , vote_average, overview , title} = movie
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie")
+
+    movieEl.innerHTML = `
+       
+        <img src="${IMGPATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class=${getClassByRate(vote_average)}>${vote_average.toFixed(1)}</span>
+        </div>
+        <div class="overview">
+        <h3>Overview</h3>
+          ${overview}
+        </div>
+ 
+        `;
+
+
+        main.appendChild(movieEl)
+  });
+
+}
+
+function getClassByRate(vote)
+{
+if(vote >= 8){
+  return 'green'
+}else if(vote >=5){
+  return 'orange'
+}else{
+return 'red'
+}
+}
+
+form.addEventListener("submit",(e)=>{
+  e.preventDefault()
+const searchTerm = search.value;
+if(searchTerm){
+
+  getMovies(SEARCHAPI + searchTerm)
+
+
+  search.value = ''
+}
+})
+
+
+
+
+
+
+
+
+
+{/* <div class="movie">
+            <img src="https://image.tmdb.org/t/p/w1280/rktDFPbfHfUbArZ6OOOKsXcv0Bm.jpg" alt="image">
+            <div class="movie-info">
+                <h3>Movie Title</h3>
+                <span>8.4</span>
+            </div>
+        </div> */}
